b2462ec8e1f5d5c8e8623de72c70a98964209702
Rajeswari Subramanigit config --global user.email s.rajeesri@gmail.com <s.rajeesri@gmail.com>
2024-10-27 23:06:09 +0530

    Rajeswari_Changes

--- src/main/java/sprint1/TestCase_Rajeswari.java
+++ src/main/java/sprint1/TestCase_Rajeswari.java
@@ -1,40 +1,34 @@
 package sprint1;
 
 import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.time.Duration;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class TestCase_Rajeswari {
 	@Test
 	public void test() throws InterruptedException, MalformedURLException {
-		/*ChromeOptions options = new ChromeOptions();
+		ChromeOptions options = new ChromeOptions();
 		options.setExperimentalOption("excludeSwitches", new String[] { "enable-automation" });
 		options.addArguments("--disable-notifications");
 		ChromeDriver driver = new ChromeDriver(options);
-		driver.manage().window().maximize();*/
-		
-		DesiredCapabilities dc =
-				new DesiredCapabilities();
-				                dc.setBrowserName("chrome");
-				                dc.setPlatform(Platform.
-				LINUX);
-				                RemoteWebDriver driver = 
-				new RemoteWebDriver(
-				new URL("http://20.40.48.160:4444/wd/hub"), dc);
+		driver.manage().window().maximize();
+
+		/*
+		 * DesiredCapabilities dc = new DesiredCapabilities();
+		 * dc.setBrowserName("chrome"); dc.setPlatform(Platform. LINUX); RemoteWebDriver
+		 * driver = new RemoteWebDriver( new URL("http://20.40.48.160:4444/wd/hub"),
+		 * dc);
+		 */
 
 		// Navigate to https://www.nba.com/stats
 		driver.get("https://www.nba.com/stats");
@@ -58,23 +52,31 @@ public class TestCase_Rajeswari {
 
 		// Choose 'Season Segment' as 'Last Game'
 		driver.findElement(By.xpath("(//option[text()='Last Game'])")).click();
-		
 
-		// Click on the player name with lowest age		
-		JavascriptExecutor je = (JavascriptExecutor) driver;
-		WebElement tableElement = driver.findElement(By.xpath("//a[text()='Jarace Walker']"));	
-		je.executeScript("arguments[0].scrollIntoView(true);", tableElement);
-		// List<WebElement> columns = tableElement.findElements(By.xpath("//th[text()='Age']"));
+		// Click on the player name with lowest age
+		Thread.sleep(3000);
+		List<WebElement> Listofage = driver.findElements(By.xpath("(//th[text()='Age'])//ancestor::table//td[4]"));
+		List<String> ages = new ArrayList<>();
+		for (WebElement element : Listofage) {
+			ages.add(element.getText());
+		}
+		Collections.sort(ages);
+		System.out.println("Lowest age in the list is " + ages.get(0));
 
 		// Click on the Profile
-		WebElement element = driver.findElement(By.xpath("//a[text()='Jarace Walker']"));
-		driver.executeScript("arguments[0].click();", element);
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		WebElement player = wait.until(ExpectedConditions
+				.elementToBeClickable(By.xpath("(//th[text()='Age'])//ancestor::table//td[4][contains(text(),'"
+						+ ages.get(0) + "')]//preceding-sibling::td[2]")));
+		player.click();
+
 		// Get the Experience of the player
-		 String experience = driver.findElement(By.xpath("(//p[text()='EXPERIENCE']//following::p)[1]")).getText();
+		String experience = driver.findElement(By.xpath("(//p[text()='EXPERIENCE']//following::p)[1]")).getText();
+		System.out.println(experience);
+
 		// Verify the player experience as 1.
-		 String expected = new String ("1 Year");
-		 System.out.println(experience.contains(expected));
+		String expected = new String("1 Year");
+		System.out.println(experience.contains(expected));
 
 	}
 }
-
